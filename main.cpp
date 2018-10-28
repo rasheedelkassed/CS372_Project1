3b5bc5a112e3087b72b519430a70008ccf1a40d5
RasheedElKassed <relkassed@gmail.com>
2018-10-28 10:28:04 -0700

    Testing data sending

--- main.cpp
+++ main.cpp
@@ -15,16 +15,22 @@
 int main() {
     struct addrinfo hints, *res;
 	int sockfd;
+	
+	char *msg = "This might work";
+	int len = strlen(msg);
+
 	// first, load up address structs with getaddrinfo():
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	getaddrinfo("flip3.engr.oregonstate.edu", "5000", &hints, &res);
+	getaddrinfo("flip2.engr.oregonstate.edu", "5000", &hints, &res);
 	// make a socket:
 	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	// bind
 	bind(sockfd, res->ai_addr, res->ai_addrlen);
 	// connect!
 	connect(sockfd, res->ai_addr, res->ai_addrlen);
+	
+	int send(int sockfd, const void *msg, int len, int flags);
 
 }
